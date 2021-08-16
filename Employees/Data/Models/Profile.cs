92221e42d8f31206248ad7664bbcc17b22d73097
Arthur Caviquioli <arc.arthur@hotmail.com>
2021-08-16 15:46:06 -0300

    colocando public

--- Employees/Data/Models/Profile.cs
+++ Employees/Data/Models/Profile.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Data.Models
 {
-    class Profile : Base
+    public class Profile : Base
     {
         public string Name { get; set; }
         public string CPF { get; set; }
