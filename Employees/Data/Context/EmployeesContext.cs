c93274d7c983951a7c0c14f6b8b379ce477e4a2a
Gabriel Maurici <gabriel.maurici@hotmail.com>
2021-08-16 16:12:58 -0300

    Add pasta Context com Class, Instalado Entity, Enable-Migrations

---
+++ Employees/Data/Context/EmployeesContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    class EmployeesContext : DbContext
+    {
+        public EmployeesContext() : base("Data Source = 192.168.0.172; Initial Catalog = Employees; Persist Security Info=True;User ID = Employees; Password=***********")
+        {
+
+        }
+    }
+}
