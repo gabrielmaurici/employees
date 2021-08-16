8ac45158ee778dd61598d1e481a131384e13660b
Rodolfo Oliveira <rodolfo.oliveira.rod@gmail.com>
2021-08-16 16:30:50 -0300

    Migration de EmployeesInvites Adicionado

---
+++ Employees/Data/Migrations/202108161930118_CreateTableEmployeesInvites.cs
@@ -0,0 +1,40 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTableEmployeesInvites : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.EmployeesInvites",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        State = c.String(),
+                        Description = c.String(),
+                        Fullname = c.String(),
+                        EmailAdress = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            DropTable("dbo.Teams");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Teams",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        AmountPeople = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            DropTable("dbo.EmployeesInvites");
+        }
+    }
+}
