3264c7d1a3878faa8eedd50e742a3b8e9b01a6cd
Arthur Caviquioli <arc.arthur@hotmail.com>
2021-08-18 08:04:50 -0300

    Repository finalizado

---
+++ Employees/Data/Repository/BaseRepository.cs
@@ -0,0 +1,57 @@
+﻿using Data.Context;
+using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    public abstract class BaseRepository <T> where T : Base
+    {
+        public void Create(T model)
+        {
+            using (var context = new EmployeesContext())
+            {
+                context.Set<T>().Add(model);
+                context.SaveChanges();
+            }
+        }
+        public List<T> Read()
+        {
+            List<T> Lista = new List<T>();
+            using (var context = new EmployeesContext())
+            {
+                Lista = context.Set<T>().ToList();
+            }
+            return Lista;
+        }
+        public T Read(int id)
+        {
+            T model;
+            using (var context = new EmployeesContext())
+            {
+                model = context.Set<T>().Find(id);
+            }
+            return model;
+        }
+        public void Update(T model)
+        {
+            using (var context = new EmployeesContext())
+            {
+                context.Entry<T>(model).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void Delete(int id)
+        {
+            using (var context = new EmployeesContext())
+            {
+                context.Entry<T>(this.Read(id)).State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+    }
+}
